5363b504ab47c1f989bd2ca6c8debc36d21f02c8
Ray Jiang <jiangrui.tr2008@gmail.com>
2012-11-13 19:55:32 +0800

    add java tutorial

---
+++ java/tutorial/examples/JavaStudyExamples/src/com/java/examples/JavaMain.java
@@ -0,0 +1,16 @@
+package com.java.examples;
+
+import java.util.ArrayList;
+
+public class JavaMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("test");
+		System.out.println("main1");
+	}
+
+}
---
+++ java/tutorial/examples/JavaStudyExamples/src/com/java/examples/JavaMain2.java
@@ -0,0 +1,12 @@
+package com.java.examples;
+
+public class JavaMain2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main2");
+	}
+}
