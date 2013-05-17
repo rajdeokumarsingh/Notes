896e9048665a53b6d83131ba87fa403f6d5a64de
jiangrui <jiangrui@jiangrui-Lenovo-IdeaPad-Y400.(none)>
2013-05-17 14:06:07 +0800

    update

---
+++ android/android.audio.system/4.2/hal/interface.cpp
@@ -0,0 +1,10 @@
+hardware/libhardware/include/hardware/audio.h
+// 参见UML class diagram: Audio HAL 1
+
+
+/* common audio stream parameters and operations */
+audio_stream/audio_stream_t
+对应什么逻辑对象： 一个output
+
+
+audio
