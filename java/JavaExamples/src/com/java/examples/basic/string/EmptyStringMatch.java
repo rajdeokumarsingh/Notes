c36995b11bccb43b7bf0291d73a4cb03b6fe33ab
Ray Jiang <jiangrui.tr2008@gmail.com>
2013-07-09 14:53:34 +0800

    update

---
+++ java/JavaExamples/src/com/java/examples/basic/string/EmptyStringMatch.java
@@ -0,0 +1,12 @@
+package com.java.examples.basic.string;
+
+public class EmptyStringMatch {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("whether".contains(""));
+        System.out.println("whether".startsWith(""));
+        System.out.println("whether".endsWith(""));
+    }
+}
