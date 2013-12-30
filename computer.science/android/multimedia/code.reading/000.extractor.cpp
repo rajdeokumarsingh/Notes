617bb4e16b19c545d84097df3eb6345ea2ed9ab0
Ray Jiang <jiangrui.tr2008@gmail.com>
2013-12-30 19:26:28 +0800

    update android multimedia

---
+++ computer.science/android/multimedia/code.reading/000.extractor.cpp
@@ -0,0 +1,25 @@
+todo: decoder
+
+Sinff in DataSourc
+    SniffWAV, SniffMP3
+    SniffMPEG4,SniffFragmentedMP4,
+    SniffMatroska, SniffOgg, 
+    SniffFLAC, SniffAMR, SniffMPEG2
+    SniffAAC,SniffMPEG2PS
+    SniffWVM
+
+MediaExtractor
+
+    MetaData
+
+    DataSource
+    
+    sp<MediaExtractor> MediaExtractor::Create(
+            const sp<DataSource> &source, const char *mime) {
+    }
+
+
+
+
+
+
