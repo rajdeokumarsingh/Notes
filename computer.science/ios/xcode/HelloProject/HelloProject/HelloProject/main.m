84cceb94c3711c49b7f937508771c729008ec8a9
Ray Jiang <jiangrui.tr2008@gmail.com>
2014-01-12 09:33:53 +0800

    Initial Commit

---
+++ computer.science/ios/xcode/HelloProject/HelloProject/HelloProject/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HelloProject
+//
+//  Created by Jiang Rui on 14-1-12.
+//  Copyright (c) 2014年 Jiang Rui. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
